0b4f1a5fb3d8ef591d144b88fb8a06cdb4abcf72
Stanislav Berrigan <valveunion76@gmail.com>
2023-06-04 01:11:00 +0300

    Shorter version

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,17 +1,8 @@
 // Use static import
-import * as mod from './wasm/wasm_mod.js';
+import initWasmModule, { hello_background } from './wasm/wasm_mod.js';
 
 
 (async () => {
-    // Manually load the Wasm module as ArrayBuffer
-    const response = await fetch('./wasm/wasm_mod_bg.wasm');
-    const buffer   = await response.arrayBuffer();
-
-    // In addition to the default initialization function that we used in the external script, 
-    // the "wasm_mod.js" exports the initSync function, which accepts a Wasm ArrayBuffer
-    const { hello_background } = mod.initSync(buffer);
-
+    await initWasmModule();
     hello_background();
 })();
-
-// wasm-pack build --release --target web
